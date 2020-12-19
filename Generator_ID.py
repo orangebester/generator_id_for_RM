23f8a5fcea8d33c706024f5f6998a84af61cef83
Vitalii <orangebester@gmail.com>
2020-12-19 12:25:45 +0200

    Editing script due to new TZ

--- Generator_ID.py
+++ Generator_ID.py
@@ -1,41 +1,52 @@
 import requests
+import sys
 import random
-import xlsxwriter
+import json
 
 
 alphabet = list('abcdefghijklmnopqrstuvwxyz')
-workbook = xlsxwriter.Workbook("New_ID.xlsx")
-bold = workbook.add_format({"bold": True})
 
 
 def generate_n_id(n):
+    """ And checks whether generated id is unique """
     temp_storage = []
     while len(temp_storage) < n:
         final_id = generate_id()
-        response = requests.get('https://www.renovationmap.org/building/'+ final_id)
+        response = requests.get(
+            'https://www.renovationmap.org/building/' + final_id)
         if final_id not in temp_storage and response.status_code != 200:
             temp_storage.append(final_id)
     return temp_storage
 
 
 def generate_id():
-    first = random.randint(0,9)
+    """ Generates id each elements of which consist of random values """
+    first = random.randint(0, 9)
     second = random.choice(alphabet)
     third = random.choice(alphabet)
     forth = random.choice(alphabet)
-    fifth = random.randint(0,9)
-    not_final_id = str(first) + second.upper() + third + forth.upper() + str(fifth)
+    fifth = random.randint(0, 9)
+    not_final_id = str(first) + second.upper() + \
+        third + forth.upper() + str(fifth)
     return not_final_id
 
 
 def general_func(n):
-    worksheet = workbook.add_worksheet()
-    worksheet.write(0, 0, "Новий ID:", bold)
-    final_storage = generate_n_id(n)
-    for i, element in enumerate(final_storage, start = 2):
-        worksheet.write(f'A{i}', element)
+    """ Write results of a script in json/txt file """
+    file1 = sys.argv[2]
+    if file1.endswith('.txt'):
+        final_storage = generate_n_id(n)
+        with open(file1, "w") as file2:
+            file2.write("Новий ID: \n")
+            for element in final_storage:
+                file2.writelines(element+"\n")
+    elif file1.endswith('.json'):
+        final_storage = dict(enumerate(generate_n_id(n), start=1))
+        with open(file1, "w") as file2:
+            json.dump(final_storage, file2)
+    else:
+        print('Extension of a file is incorrect.\nPlease use file, which extension is ".json" or ".text"')
+        sys.exit()
 
 
-
-general_func(2)
-workbook.close()
+general_func(int(sys.argv[1]))
